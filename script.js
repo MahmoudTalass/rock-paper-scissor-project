1e9b57d6e174abad6e727ff79477ed95575955b1
Mahmoud <mahmoud.talass@gmail.com>
2022-11-19 15:43:35 -0500

    Fix the game function and make other changes
    1.change the location of score variable to the top of the script so it is
    in the global scope.
    2.Move the prompt that asks for player selection from the playRound
    function to the game function inside the for loop.
    3.in the game function create an if statement that checks if the
    player's score and annouces the player as a winner if the score is 3 or
    more and a loser if the score is less.
    4.call the game function once

--- script.js
+++ script.js
@@ -1,37 +1,78 @@
-// Create getComputerChoice
+// create variable to keep track of player's score
+let score = 0;
+
+
+
+// Create getComputerChoice() function
 // the function randomly returns either 'Rock', 'Paper' or 'Scissors'.
 // create an array inside the function that contains the possible return values.
 // Use random number function to select a random index between 0 and 2
 // Return the value of the random index
 
 function getComputerChoice() {
-   const gameOptions = ["Rock", "Paper", "Scissors"];
+   const gameOptions = ["rock", "paper", "scissors"];
    let option = Math.floor(Math.random() * gameOptions.length);
+   console.log(gameOptions[option] + " - computer")
    return gameOptions[option];
 }
 
-// Create the playRound function that plays a single round of Rock Paper Scissors.
+// Create the playRound() function that plays a single round of Rock Paper Scissors.
 // The function should take two parameters, playerSelection and computerSelection.
 // Make sure that the playerSelection parameter is case-insensitve
-// The function should return a string that declares the winnder of the round
+// declare a result variable that stores the winning/losing message of the round
+// if the player wins, increment score
+// return the result variable
 
 function playRound(playerSelection, computerSelection) {
-   playerSelection = playerSelection.toLowerCase();
-   computerSelection = computerSelection.toLowerCase();
-
-   if (playerSelection == "rock" && computerSelection == "paper") {
-      return "You Lose! Paper beats Rock";
-   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-      return "You Win! Rock beats Scissors";
-   } else if (playerSelection == "paper" && computerSelection == "rock") {
-      return "You Win! Paper beats Rock";
-   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-      return "You Lose! Scissors beats Paper ";
-   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-      return "You Lose! Rock beats Scissors";
-   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-      return "You Win! Scissors beats Paper";
+   let result;
+   if (playerSelection === "rock" && computerSelection === "paper") {
+      result = "You Lose! Paper beats Rock";
+      console.log(result)
+      return result;
+   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      result = "You Win! Rock beats Scissors";
+      console.log(result);
+      score++;
+      return result;
+   } else if (playerSelection === "paper" && computerSelection === "rock") {
+      result = "You Win! Paper beats Rock";
+      console.log(result);
+      score++
+      return result;
+   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+      result = "You Lose! Scissors beats Paper";
+      console.log(result);
+      return result;
+   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      result = "You Lose! Rock beats Scissors";
+      console.log(result);
+      return result;
+   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+      result = "You Win! Scissors beats Paper";
+      console.log(result);
+      score++;
+      return result;
    } else {
-    return "Its a draw!"
+      result = "Draw";
+      console.log(result);
+      return result;
    }
 }
+
+//Create the function game()
+//The function calls playRound() function 5 times through a forLoop
+//The function keeps track of the wins in each round and declares the winner at the end of the 5 rounds
+
+function game() {
+   for (let i = 1; i <= 5; i++) {
+      let playerSelection = prompt("Rock, paper, or scissors?");
+      playerSelection = playerSelection.toLowerCase();
+      playRound(playerSelection, getComputerChoice());
+}
+   if (score >= 3) {
+      console.log("congraduations, you won with a score of " + score)
+   } else {
+      console.log("you lost :(")
+   }
+}
+game();
